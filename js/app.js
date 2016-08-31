0169b1ee96775e4d4048137106824f7de5ad4fdd
Uğur Arıcı <m.ugurarici@gmail.com>
2016-08-31 23:22:37 +0300

    Remove countdown javascript codes from app I belive that was unnecessary for main-header area. We need a register button there. Maybe someone can add that counter another place. Till that, i removed counters js codes too.

--- js/app.js
+++ js/app.js
@@ -4,16 +4,6 @@ $(document).ready(function(){
     var WH = $(window).height();
     $('#main-header').height(WH);
 
-
-    // --- COUNTDOWN
-    $('.countdown-area').countdown("2016/10/08 09:00:00", function (event) {
-        $('.count-month').html("<span>"+event.strftime('%m')+"</span>AY");
-        $('.count-day').html("<span>"+event.strftime('%n')+"</span>GÜN");
-        $('.count-hour').html("<span>"+event.strftime('%H')+"</span>SAAT");
-        $('.count-minute').html("<span>"+event.strftime('%M')+"</span>DAKİKA");
-        $('.count-second').html("<span>"+event.strftime('%S')+"</span>SANİYE");
-    });
-
     // --- ANIMATE
     $('.go').on('click', function(e) {
         var id = $(this).attr('href');
